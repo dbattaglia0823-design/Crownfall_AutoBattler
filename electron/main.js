790e781a6cb1edcceb5a74544b9f20ee595e2b72
dbattaglia0823-design <dbattaglia0823@gmail.com>
2026-05-13 23:37:48 -0400

    Add files via upload

---
+++ electron/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 960,
+    minHeight: 620,
+    title: "Crownfall Auto Battler",
+    backgroundColor: "#120d0a",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, "..", "index.html"));
+
+  if (isDev) {
+    win.webContents.on("before-input-event", (event, input) => {
+      if (input.key === "F12" || (input.control && input.shift && input.key.toLowerCase() === "i")) {
+        win.webContents.toggleDevTools();
+      }
+    });
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
